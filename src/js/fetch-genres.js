6407b858a938fc5956b9fab5eac6cdc7fdd337e7
jairocaracas <jaercafe@hotmail.com>
2023-08-05 17:29:48 -0500

    Initial commit

---
+++ src/js/fetch-genres.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { options } from '..';
+
+async function fetchGenre() {
+  try {
+    const response = await axios.get(
+      'https://api.themoviedb.org/3/genre/movie/list?language=en',
+      options
+    );
+    return response.data.genres;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getGenreById(id) {
+  let genres = await fetchGenre();
+
+  let newArray = [];
+  let filtered = genres.map(genre => {
+    for (let i = 0; i < id.length; i++) {
+      if (genre.id === id[i]) {
+        newArray.push(genre.name);
+      }
+    }
+  });
+  // console.log(newArray);
+  return newArray;
+}
+
+export { getGenreById };
---
+++ src/js/render-movies.js
@@ -0,0 +1,26 @@
+import { getGenreById } from './fetch-genres';
+
+async function renderMovieCards(fetchFunction, div) {
+  let renderCards = await fetchFunction;
+  div.innerHTML = ' ';
+
+  let newRender = await Promise.all(
+    renderCards.map(
+      async ({ genre_ids, id, poster_path, release_date, title }) => {
+        let genre = await getGenreById(genre_ids);
+        return `<div class="movie-card" >
+          <div class="poster-container">
+       <img class="poster" id="${id}" src="https://image.tmdb.org/t/p/original${poster_path}" alt="${title}" loading="lazy"/>
+       </div>
+  <div class="info">
+  <p class="info-item">${title.toUpperCase()} <br/>
+    <span> ${genre} | ${release_date.slice(0, 4)} </span></p>
+  </div>
+  </div>`;
+      }
+    )
+  );
+  div.innerHTML += newRender.join(' ');
+}
+
+export { renderMovieCards };
