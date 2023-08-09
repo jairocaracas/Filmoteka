2482e2700bf86f27a48fdaabec50c6affbfed729
davidfsora <davidsora0204@gmail.com>
2023-08-08 21:41:49 -0500

    update search query

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import { renderMovieCards } from './js/render-movies.js';
 import { fetchTrends } from './js/fetch-trends.js';
 import { fetchMovie } from './js/fetch-movie.js';
+import { fetchData } from './js/fetch-search-value.js';
 
+let form = document.querySelector('.form');
 let moviesGallery = document.querySelector('.movies-gallery');
 
 renderMovieCards(fetchTrends(), moviesGallery);
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	renderMovieCards(fetchData(), moviesGallery);
+});
 
 moviesGallery.addEventListener('click', fetchMovie);
