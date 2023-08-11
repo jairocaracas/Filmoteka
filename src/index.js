ed183eaf48fff7526641040d392259a9a0be91ba
jairocaracas <jaercafe@hotmail.com>
2023-08-10 19:15:28 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import { renderMovieCards } from './js/render-movies.js';
 import { fetchTrends } from './js/fetch-trends.js';
 import { fetchMovie } from './js/fetch-movie.js';
+import { element } from './js/pagination.js';
 
 let moviesGallery = document.querySelector('.movies-gallery');
 
-renderMovieCards(fetchTrends(), moviesGallery);
+let listItem = document.querySelector('#pag');
+
+renderMovieCards(fetchTrends(1), moviesGallery);
+element(20, 1);
 
 moviesGallery.addEventListener('click', fetchMovie);
+
+listItem.addEventListener('click', e => {
+  let nextPage = e.target.querySelector('span').innerText;
+  console.log(nextPage);
+  renderMovieCards(fetchTrends(nextPage), moviesGallery);
+  element(20, nextPage);
+});
